1825a7221bd8f51b08aca7fe875daceb593d1b0c
yining0417 <ezreal10@connect.hku.hk>
2016-08-05 23:14:03 +0800

    try

--- dare-code/src/dare_ibv_ud.c
+++ dare-code/src/dare_ibv_ud.c
@@ -691,7 +691,6 @@ int loggp_not_inline = 0;
 static int
 ud_send_message( ud_ep_t *ud_ep, uint32_t len )
 {
-    info_wtime(log_fp, "ud_send_message");
     int rc;
     struct ibv_sge sg;
     struct ibv_send_wr wr;
@@ -729,8 +728,6 @@ ud_send_message( ud_ep_t *ud_ep, uint32_t len )
     if (0 != rc) {
         error_return(1, log_fp, "ibv_post_send failed because %s\n", strerror(rc));
     }
-    else
-        info_wtime(log_fp, "ibv_post_send success");
 
     /* Wait for send operation to complete */
     struct ibv_wc wc;
